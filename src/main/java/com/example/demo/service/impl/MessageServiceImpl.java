f7bfc87635ad201de929c31fe1f9a4e46ebe47af
tpzzz123 <22371455@buaa.edu.cn>
2024-05-26 14:53:47 +0800

    添加了一个简单的创建用户时的验证

--- src/main/java/com/example/demo/service/impl/MessageServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/MessageServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Date;
 import java.util.List;
 @Service("messageService")
 public class MessageServiceImpl implements MessageService{
